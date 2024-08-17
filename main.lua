59129245253a66569ce4c64f4562a9a1cc41a775
Jos <161704274+xthrx0@users.noreply.github.com>
2024-08-17 10:38:41 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,17 @@
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local player = Players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local auraRadius = 10
+
+local Remotes = ReplicatedStorage:WaitForChild("Remotes")
+local GibRemote = Remotes:WaitForChild("Gib")
+
 local function autoKill()
     if not character or not character:FindFirstChild("HumanoidRootPart") then return end
+
     local zombiesFolder = workspace:FindFirstChild("Zombies")
     if zombiesFolder then
         for _, Agent in pairs(zombiesFolder:GetChildren()) do
@@ -15,13 +21,18 @@ local function autoKill()
                 local distance = (character.HumanoidRootPart.Position - agentPosition).Magnitude
 
                 if distance <= auraRadius then
-                    humanoid:TakeDamage(humanoid.Health)
-                    print("Auto-killed " .. Agent.Name)
+                    local args = {
+                        [1] = player.Name,
+                        [2] = Agent,
+                        [3] = "Right Arm",
+                        [4] = agentPosition,
+                        [5] = Agent.HumanoidRootPart.CFrame.LookVector
+                    }
+                    GibRemote:FireServer(unpack(args))
                 end
             end
         end
     end
 end
-RunService.RenderStepped:Connect(function()
-    autoKill()
-end)
+
+RunService.RenderStepped:Connect(autoKill)
